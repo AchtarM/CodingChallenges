7b6bf50c33fb7d30a8d866093cfb122bf205a3e4
Mohamad Achtar <mohamad.achtar@gmail.com>
2022-03-02 16:37:42 +0200

    Create Multiplying numbers as strings.js

---
+++ Multiplying numbers as strings.js
@@ -0,0 +1,10 @@
+
+function multiply(a, b)
+{
+  //Used BigInt functio to handel the big numbers
+  return String(BigInt(a) * BigInt(b));
+}
+
+console.log(multiply("1020303004875647366210", "2774537626200857473632627613"));
+
+
